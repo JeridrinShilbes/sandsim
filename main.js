cc035bf90650cf2fcb43cffd48b7c823341281c9
Jeridrin Shilbes <jeridrinshilbesforever@gmail.com>
2026-01-31 22:21:05 +0530

    start of a sand sim using canvas to test if its better

---
+++ main.js
@@ -0,0 +1,10 @@
+const tileSize=5;
+const screen=document.getElementById("sandBox");
+const X=screen.width/tileSize;
+const Y=screen.height/tileSize;
+
+
+function main()
+{
+    const array=new Array(1000).fill(false);;
+}
